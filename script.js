b33c27101b3649417c89bd8fdd1dab09edb21ec5
Glitch (pop-the-ball) <none>
2024-11-12 00:01:22 +0000

    feat: create métodos para determinar a posição das esferas na cena

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 const cena = document.querySelector("#cena");
 let jogo = undefined;
 
+// X = -3 até 3
+function getPositionX() {
+  return (Math.random() * 6) - 3;
+}
+
+// Y = -0.5 até 2.5
+function getPositionY() {
+  return Math.random() * 3 - 0.5;
+}
+
+// Z = -1.2 até -3
+function getPositionZ() {
+  return Math.random() * (-1.2 + 3) - 3;
+}
+
 function newGame() {
   jogo = setInterval(() => {
     const ball = document.createElement("a-sphere");
     ball.setAttribute("class", "raycastable");
     ball.setAttribute("radius", "0.3");
     ball.setAttribute("color", "#333");
-    ball.setAttribute("position", "0 0 -3");
+    ball.setAttribute("position", `${getPosition()} ${getPositionY()} ${getPositionZ()}`);
     cena.appendChild(ball);
 
     ball.addEventListener("mouseenter", () => {
